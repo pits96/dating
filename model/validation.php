86fccb423f88bb2e46797334fd0f07eb02491ec0
Aaron Tadina <atadina@mail.greenriver.edu>
2020-05-27 18:34:10 -0400

    Modified validPhone() function.

--- model/validation.php
+++ model/validation.php
@@ -6,5 +6,10 @@ function validAge($num){
     return is_numeric($num);
 }
 function validPhone($num){
-    return is_numeric($num)&&sizeof($num)>4;
+    return is_numeric($num)&&strlen($num);
+}
+function validEmail($text){
+    $valid = strpos($text,'@');
+    $valid2 = strpos($text,'.com');
+    return $valid&&$valid2;
 }
