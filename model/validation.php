7175e428ffa10840d58d56edcb02ae6880e17b5f
Aaron Tadina <atadina@mail.greenriver.edu>
2020-06-04 18:21:38 -0400

    Created validation class

--- model/validation.php
+++ model/validation.php
@@ -1,28 +1,40 @@
 <?php
-function validName($text){
-    return ctype_alpha($text);
-}
-function validAge($num){
-    return is_numeric($num);
-}
-function validPhone($num){
-    return is_numeric($num)&&strlen($num);
-}
-function validEmail($text){
-    $valid = strpos($text,'@');
-    $valid2 = strpos($text,'.com');
-    return $valid&&$valid2;
-}
-function validInterest($selectedInterest,$array)
+class validation
 {
+
+    function validName($text)
+    {
+        return ctype_alpha($text);
+    }
+
+    function validAge($num)
+    {
+        return is_numeric($num);
+    }
+
+    function validPhone($num)
+    {
+        return is_numeric($num) && strlen($num);
+    }
+
+    function validEmail($text)
+    {
+        $valid = strpos($text, '@');
+        $valid2 = strpos($text, '.com');
+        return $valid && $valid2;
+    }
+
+    function validInterest($selectedInterest, $array)
+    {
 //    print_r($selectedInterest);
 //    print_r($array);
 
 //    We need to check each condiment in our array
-    foreach ($selectedInterest as $selected) {
-        if (!in_array($selected, $array)) {
-            return false;
+        foreach ($selectedInterest as $selected) {
+            if (!in_array($selected, $array)) {
+                return false;
+            }
         }
+        return true;
     }
-    return true;
 }
